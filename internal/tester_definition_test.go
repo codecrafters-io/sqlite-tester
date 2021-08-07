43e905af8eb08b9037124bd0b13a27b75ab303f6
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-08-07 17:44:00 +0100

    update course definition test

--- internal/tester_definition_test.go
+++ internal/tester_definition_test.go
@@ -1,6 +1,7 @@
 package internal
 
-// TODO: Uncomment once all stages are ready
-//func TestStagesMatchYAML(t *testing.T) {
-//	testerDefinition.TestAgainstYAML(t, "test_helpers/course_definition.yml")
-//}
+import "testing"
+
+func TestStagesMatchYAML(t *testing.T) {
+	testerDefinition.TestAgainstYAML(t, "test_helpers/course_definition.yml")
+}
